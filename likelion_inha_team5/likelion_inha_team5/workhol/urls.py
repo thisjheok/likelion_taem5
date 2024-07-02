cddf00324b1968c7659a88202ce4a5b0b73e5cb3
YeomJongSeop <bhs3435@naver.com>
2024-07-02 15:45:11 +0900

    Feat Likes api

--- likelion_inha_team5/likelion_inha_team5/workhol/urls.py
+++ likelion_inha_team5/likelion_inha_team5/workhol/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('language_study/', views.language_study_site, name='language_study_site'),
     path('intern/', views.intern_site, name='intern_site'),
     path('<str:site_name>/<str:category_name>/post/', views.create_post, name='create_post'),
+    path('pressLike/<int:pk>/', views.press_like, name='press_like'),  # 좋아요 누르기 URL 추가
 ]
