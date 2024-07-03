135d1cd82cb241b11c58de3f48ac3e5e9122d253
YeomJongSeop <bhs3435@naver.com>
2024-07-03 18:51:01 +0900

    Feat swagger

--- likelion_inha_team5/likelion_inha_team5/likelion_inha_team5/settings.py
+++ likelion_inha_team5/likelion_inha_team5/likelion_inha_team5/settings.py
@@ -1,8 +1,8 @@
 from pathlib import Path
+import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Build paths inside the project like this: BASE_DIR / 'subdir'
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -14,7 +14,6 @@ SECRET_KEY = "django-insecure-uw44o3u10@$5n!^%)-o7no9yf)em0^#bj^a4oqadc^e#hu4fp(
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -44,7 +43,7 @@ ROOT_URLCONF = "likelion_inha_team5.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR/'templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'likelion_inha_team5', 'templates')], # 템플릿 경로 추가하기
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -59,17 +58,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "likelion_inha_team5.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": str(BASE_DIR / "db.sqlite3"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -89,7 +86,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -101,11 +97,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- likelion_inha_team5/likelion_inha_team5/workhol/views.py
+++ likelion_inha_team5/likelion_inha_team5/workhol/views.py
@@ -1,24 +1,45 @@
-from django.shortcuts import render , redirect
+from django.shortcuts import render, redirect
 import json
 from django.http import JsonResponse, HttpResponse, HttpResponseForbidden
 from .models import *
 from django.shortcuts import get_object_or_404
 from django.contrib.auth import login, authenticate
-from .forms import SignUpForm
+from .forms import *
 from django.contrib.auth.forms import AuthenticationForm
 from django.utils import timezone
-from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
-from .forms import *
-
 from rest_framework.decorators import api_view
-from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
 from rest_framework import status
-
 from .serializer import *
+from drf_yasg.utils import swagger_auto_schema
 
+# 사이트 이름과 카테고리 이름 매핑
+SITE_NAME_MAPPING = {
+    'intern': '해외취업',
+    'language-study': '어학연수',
+    'working-holiday': '워킹홀리데이',
+}
+
+CATEGORY_NAME_MAPPING = {
+    'community': '커뮤니티',
+    'group-buying': '공구',
+    'agency-document': '대행, 서류작성',
+    'info': '정보',
+}
+
+# 회원가입
+@swagger_auto_schema(
+    method="post",
+    tags=["회원가입"],
+    operation_summary="회원가입",
+    operation_description="회원가입을 처리합니다.",
+    responses={
+        201: '회원가입 성공',
+        400: '잘못된 요청',
+        500: '서버 오류'
+    }
+)
+@api_view(['GET', 'POST'])
 def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
@@ -36,6 +57,19 @@ def signup(request):
         form = SignUpForm()
     return render(request, 'workhol/signup.html', {'form': form})
 
+# 로그인
+@swagger_auto_schema(
+    method="post",
+    tags=["로그인"],
+    operation_summary="로그인",
+    operation_description="로그인을 처리합니다.",
+    responses={
+        200: '로그인 성공',
+        400: '잘못된 요청',
+        500: '서버 오류'
+    }
+)
+@api_view(['GET', 'POST'])
 def login_view(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
@@ -52,33 +86,79 @@ def login_view(request):
         form = LoginForm()
     return render(request, 'workhol/login.html', {'form': form})
 
+# 홈
+@swagger_auto_schema(
+    method="get",
+    tags=["홈"],
+    operation_summary="홈 페이지",
+    operation_description="홈 페이지를 렌더링합니다.",
+    responses={
+        200: '홈 페이지 렌더링 성공',
+        500: '서버 오류'
+    }
+)
+@api_view(['GET'])
 def home(request):
     return render(request, 'workhol/home.html')
 
+# 워킹홀리데이 사이트
+@swagger_auto_schema(
+    method="get",
+    tags=["워킹홀리데이"],
+    operation_summary="워킹홀리데이 사이트",
+    operation_description="워킹홀리데이 사이트를 렌더링합니다.",
+    responses={
+        200: '워킹홀리데이 사이트 렌더링 성공',
+        500: '서버 오류'
+    }
+)
+@api_view(['GET'])
 def workhol_site(request):
     return render(request, 'workhol/workhol_site.html')
 
+# 언어학습 사이트
+@swagger_auto_schema(
+    method="get",
+    tags=["언어학습"],
+    operation_summary="언어학습 사이트",
+    operation_description="언어학습 사이트를 렌더링합니다.",
+    responses={
+        200: '언어학습 사이트 렌더링 성공',
+        500: '서버 오류'
+    }
+)
+@api_view(['GET'])
 def language_study_site(request):
     return render(request, 'workhol/language_study_site.html')
 
+# 인턴 사이트
+@swagger_auto_schema(
+    method="get",
+    tags=["인턴"],
+    operation_summary="인턴 사이트",
+    operation_description="인턴 사이트를 렌더링합니다.",
+    responses={
+        200: '인턴 사이트 렌더링 성공',
+        500: '서버 오류'
+    }
+)
+@api_view(['GET'])
 def intern_site(request):
     return render(request, 'workhol/intern_site.html')
 
-
-# 사이트 이름과 카테고리 이름 매핑
-SITE_NAME_MAPPING = {
-    'intern': '해외취업',
-    'language-study': '어학연수',
-    'working-holiday': '워킹홀리데이',
-}
-
-CATEGORY_NAME_MAPPING = {
-    'community': '커뮤니티',
-    'group-buying': '공구',
-    'agency-document': '대행, 서류작성',
-    'info': '정보',
-}
-
+# 게시물 생성
+@swagger_auto_schema(
+    method="post",
+    tags=["게시물"],
+    operation_summary="게시물 생성",
+    operation_description="새로운 게시물을 생성합니다.",
+    responses={
+        201: '게시물 생성 성공',
+        400: '잘못된 요청',
+        500: '서버 오류'
+    }
+)
+@api_view(['GET', 'POST'])
 def create_post(request, site_name, category_name):
     initial_continents = [
         ('AS', '아시아'),
@@ -90,7 +170,6 @@ def create_post(request, site_name, category_name):
         ('ME', '중동')
     ]
 
-    # Continent 객체가 없을 경우 생성
     if not Continent.objects.exists():
         for code, name in initial_continents:
             Continent.objects.create(continent_name=code)
@@ -100,7 +179,7 @@ def create_post(request, site_name, category_name):
     
     site, _ = Site.objects.get_or_create(site_name=site_name)
     category, _ = Category.objects.get_or_create(category_name=category_name)
-    site_category,_ =SiteCategory.objects.get_or_create(site=site,category=category)
+    site_category,_ = SiteCategory.objects.get_or_create(site=site, category=category)
 
     if request.method == 'POST':
         form = PostForm(request.POST, request.FILES)
@@ -125,19 +204,57 @@ def create_post(request, site_name, category_name):
     }
     return render(request, 'workhol/create_post.html', context)
 
-
+# 게시물 목록
+@swagger_auto_schema(
+    method="get",
+    tags=["게시물"],
+    operation_summary="게시물 목록",
+    operation_description="게시물 목록을 가져옵니다.",
+    responses={
+        200: '게시물 목록 가져오기 성공',
+        500: '서버 오류'
+    }
+)
+@api_view(['GET'])
 def post_list(request, site_name, category_name):
     site = get_object_or_404(Site, site_name=site_name)
     category = get_object_or_404(Category, category_name=category_name)
     posts = Post.objects.filter(site=site, category=category)
     return render(request, 'workhol/post_list.html', {'posts': posts, 'site_name': site_name, 'category_name': category_name})
 
+# 게시물 상세보기
+@swagger_auto_schema(
+    method="get",
+    tags=["게시물"],
+    operation_summary="게시물 상세보기",
+    operation_description="게시물 상세 정보를 가져옵니다.",
+    responses={
+        200: '게시물 상세보기 성공',
+        404: '게시물 찾을 수 없음',
+        500: '서버 오류'
+    }
+)
+@api_view(['GET'])
 def post_detail(request, site_name, category_name, id):
     site = get_object_or_404(Site, site_name=site_name)
     category = get_object_or_404(Category, category_name=category_name)
     post = get_object_or_404(Post, site=site, category=category, id=id)
     return render(request, 'workhol/post_detail.html', {'post': post})
 
+# 게시물 수정
+@swagger_auto_schema(
+    method="post",
+    tags=["게시물"],
+    operation_summary="게시물 수정",
+    operation_description="게시물을 수정합니다.",
+    responses={
+        200: '게시물 수정 성공',
+        403: '권한 없음',
+        404: '게시물 찾을 수 없음',
+        500: '서버 오류'
+    }
+)
+@api_view(['GET', 'POST'])
 def post_update(request, site_name, category_name, id):
     post = get_object_or_404(Post, id=id, site__site_name=site_name, category__category_name=category_name)
     if request.user != post.author:
@@ -153,20 +270,43 @@ def post_update(request, site_name, category_name, id):
     
     return render(request, 'workhol/post_update.html', {'form': form})
 
+# 게시물 삭제
+@swagger_auto_schema(
+    method="post",
+    tags=["게시물"],
+    operation_summary="게시물 삭제",
+    operation_description="게시물을 삭제합니다.",
+    responses={
+        200: '게시물 삭제 성공',
+        403: '권한 없음',
+        404: '게시물 찾을 수 없음',
+        500: '서버 오류'
+    }
+)
+@api_view(['POST'])
 def post_delete(request, site_name, category_name, id):
     post = get_object_or_404(Post, id=id, site__site_name=site_name, category__category_name=category_name)
     if request.user != post.author:
         return HttpResponseForbidden("You are not allowed to delete this post")
 
     if request.method == 'POST':
-        # HTML에서는 DELETE 메서드를 직접 사용할 수 X
-        # 그래서 POST 메서드로 삭제 요청함
         post.delete()
         return redirect('post_list', site_name=site_name, category_name=category_name)
     
     return render(request, 'workhol/post_confirm_delete.html', {'post': post})
 
 # 좋아요 누르기 기능 추가
+@swagger_auto_schema(
+    method="patch",
+    tags=["게시물"],
+    operation_summary="좋아요 누르기",
+    operation_description="게시물에 좋아요를 누릅니다.",
+    responses={
+        200: '좋아요 성공',
+        404: '게시물 찾을 수 없음',
+        500: '서버 오류'
+    }
+)
 @api_view(['PATCH'])
 def press_like(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -174,10 +314,20 @@ def press_like(request, pk):
     post.save()
     return Response({'message': f'{pk}의 총 좋아요 수는 {post.likes}입니다.'}, status=status.HTTP_200_OK)
 
-
-
 # 댓글 작성 기능 추가
-def create_comments(request,pk):
+@swagger_auto_schema(
+    method="post",
+    tags=["댓글"],
+    operation_summary="댓글 작성",
+    operation_description="게시물에 댓글을 작성합니다.",
+    responses={
+        201: '댓글 작성 성공',
+        400: '잘못된 요청',
+        500: '서버 오류'
+    }
+)
+@api_view(['GET', 'POST'])
+def create_comments(request, pk):
     post = get_object_or_404(Post, pk=pk)
     comments = post.comments.all()
     if request.method == 'POST':
@@ -187,7 +337,7 @@ def create_comments(request,pk):
             comments.post = post
             request.user.point += 10
             comments.save()
-            return redirect('create_comments',pk=pk)
+            return redirect('create_comments', pk=pk)
         else:
             print(form.errors)
     else:
@@ -200,21 +350,47 @@ def create_comments(request,pk):
     return render(request, 'workhol/create_comments.html', context)
 
 # 댓글 삭제 기능 추가
+@swagger_auto_schema(
+    method="post",
+    tags=["댓글"],
+    operation_summary="댓글 삭제",
+    operation_description="게시물의 댓글을 삭제합니다.",
+    responses={
+        200: '댓글 삭제 성공',
+        403: '권한 없음',
+        404: '댓글 찾을 수 없음',
+        500: '서버 오류'
+    }
+)
+@api_view(['POST'])
 def delete_comments(request, pk):
     if request.user != comments.author:
         return HttpResponseForbidden("You are not allowed to delete this post")
     if request.method == 'POST':
-        comments=get_object_or_404(Comments, pk=pk)
+        comments = get_object_or_404(Comments, pk=pk)
         comments.delete()
-        return JsonResponse({'message':"댓글이 삭제되었습니다"})
+        return JsonResponse({'message': "댓글이 삭제되었습니다"})
     
 # 댓글 수정 기능 추가
+@swagger_auto_schema(
+    method="post",
+    tags=["댓글"],
+    operation_summary="댓글 수정",
+    operation_description="게시물의 댓글을 수정합니다.",
+    responses={
+        200: '댓글 수정 성공',
+        403: '권한 없음',
+        404: '댓글 찾을 수 없음',
+        500: '서버 오류'
+    }
+)
+@api_view(['POST'])
 def update_comments(request, pk):
     if request.user != comments.author:
         return HttpResponseForbidden("You are not allowed to delete this post")
     if request.method == 'POST':
-        comments=get_object_or_404(Comments,pk=pk)
-        data=json.loads(request.body)
-        comments.content=data.get('content')
+        comments = get_object_or_404(Comments, pk=pk)
+        data = json.loads(request.body)
+        comments.content = data.get('content')
         comments.save()
-        return JsonResponse({"message":'댓글이 수정되었습니다'})
+        return JsonResponse({"message": '댓글이 수정되었습니다'})
